185975d90ef22a93f0442a2f1fb44335475ebd28
mirza <sugamirza2@gmail.com>
2019-03-31 19:45:38 +0700

    no message

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const port = process.env.PORT || 80;
-// const host = process.env.HOST || '127.0.0.1';
+const host = process.env.HOST || '206.189.83.45';
 app = express();
 app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function(req,res) {
   res.sendfile(__dirname + '/dist/index.html');
 });
-app.listen(port);
+app.listen(port,host);
 console.log(`SERVER STARTED on PORT ${port}`);
