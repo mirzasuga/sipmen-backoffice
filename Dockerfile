1a439ee03df7f037c524e083e2d639a8bd06cd8b
mirza <sugamirza2@gmail.com>
2019-03-31 19:10:11 +0700

    dockerize

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:lts-alpine as build-stage
+WORKDIR /app
+
+# production stage
+# FROM nginx:stable-alpine as production-stage
+# COPY --from=build-stage /app/dist /usr/share/nginx/html
+
+COPY package*.json ./
+RUN npm install --silent
+COPY . .
+RUN npm run build
+
+EXPOSE 80
+# CMD ["nginx", "-g", "daemon off;"]
+CMD ["npm", "start"]
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 80;
+// const host = process.env.HOST || '127.0.0.1';
 app = express();
 app.use(express.static(__dirname + '/dist/'));
 app.get(/.*/, function(req,res) {
   res.sendfile(__dirname + '/dist/index.html');
 });
 app.listen(port);
-console.log(`SERVER STARTED on PORT: ${port}`);
+console.log(`SERVER STARTED on PORT ${port}`);
